6b916f3a07dc18def39607ffd8b3013152986eb3
Ruslan Bogatyrenko <ruslan.bogatyrenko@gmail.com>
2022-12-24 19:52:29 +0200

    Add m3u scanner

---
+++ scan_music.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os
+import glob
+
+dir = "/media/storage/Music"
+
+def scan_for_palylist():
+    for (path, subdirs, files) in os.walk(dir):
+        os.chdir(path)
+        if glob.glob("*.mp3") != []:
+            _m3u = open( "/home/neuro/" + os.path.split(path)[1] + ".m3u" , "w" )
+            print(os.path.split(path))
+            for song in glob.glob("*.mp3"):
+                _m3u.write(song + "\n")
+            _m3u.close()
+    #os.chdir(dir) # Not really needed..
+
+
+def main():
+   scan_for_palylist()
+
+if __name__ == '__main__':
+    main()
+
+
