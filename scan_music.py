2d8fc0c749fc02ba5e1a25c05770ee8f237d67ad
Ruslan Bogatyrenko <ruslan.bogatyrenko@gmail.com>
2023-01-01 14:42:22 +0200

    update

--- scan_music.py
+++ scan_music.py
@@ -8,10 +8,13 @@ def scan_for_palylist():
     for (path, subdirs, files) in os.walk(dir):
         os.chdir(path)
         if glob.glob("*.mp3") != []:
+            cur_dir = os.path.split(path)
             _m3u = open( "/home/neuro/" + os.path.split(path)[1] + ".m3u" , "w" )
             #print(os.path.split(path))
+            _m3u.write("#EXTM3U\n")
             for song in glob.glob("*.mp3"):
-                _m3u.write(song + "\n")
+                _m3u.write("#EXTINF:0,"+ song + "\n")
+                _m3u.write(dir + "/" + cur_dir[1] + "/" + song + "\n")
             _m3u.close()
     #os.chdir(dir) # Not really needed..
 
